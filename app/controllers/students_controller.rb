ccacb847f9cc7082b265d3b1f59781b37e2fbac0
Andrew Smith <848smith@gmail.com>
2020-06-17 01:19:15 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,8 +13,10 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-    @student
-    
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @studnet.save
+    redirect_to 
   end
 
 end
